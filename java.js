d1a4800f483f6aa8b11259cf094639aeb5b86379
UnboundKey <godiset@gmail.com>
2017-05-11 07:50:57 +0200

    Improved JS and slimed downloads a bit

--- java.js
+++ java.js
@@ -1,14 +1,5 @@
-function neshide() {
-    var x = document.getElementById('Neslist');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-function sneshide() {
-    var x = document.getElementById('sneslist');
+function showhide(id) {
+    var x = document.getElementById(id);
     if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
