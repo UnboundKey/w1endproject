3fbe6090be433c142a13338b09c2ef9dfa0e3199
UnboundKey <godiset@gmail.com>
2017-05-11 07:43:34 +0200

    Initial Commit
    Fist commit of the bunch

---
+++ java.js
@@ -0,0 +1,17 @@
+function neshide() {
+    var x = document.getElementById('Neslist');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function sneshide() {
+    var x = document.getElementById('sneslist');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
